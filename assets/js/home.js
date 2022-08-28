f8357b2c09997db30b547ea530a6596872560078
CyberAli1989 <naserianali@gmail.com>
2022-08-28 19:33:41 +0430

    add mobile nav and desktop nav

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,14 +1,40 @@
 $(function () {
-  $(window).scroll(function (e) {
-      console.log($(window).scrollTop());
-      if ($(window).scrollTop()>50 && $(window).scrollTop() <= 130){
-          $("nav#navigation").hide();
-      }
-      if ($(window).scrollTop() > 100){
-        $("nav#navigation").show("fast").slideDown(400).addClass("stick");
-    }else{
-        $("nav#navigation").removeClass("stick").show();
+    $("#goTop").hide();
+    //  sticky navbar ;
+    $(window).scroll(function (e) {
+        console.log($(window).scrollTop());
+        if ($(window).scrollTop() > 50 && $(window).scrollTop() <= 130) {
+            $("nav#navigation").hide();
+        }
+        if ($(window).scrollTop() > 100) {
+            $("nav#navigation").show("fast").slideDown(400).addClass("stick");
+            $("#goTop").show();
+        } else {
+            $("nav#navigation").removeClass("stick").show();
+            $("#goTop").hide();
 
-    }
-  })
+        }
+    });
+    // mobile navbar
+
+    let isOpen = false;
+    $("#mobileMenu").on("click", function (e) {
+        if (!isOpen){
+            $("ul.nav").slideDown();
+            $(this).removeClass("fa-navicon").addClass("fa-close");
+            $("nav").addClass("mobileMenu");
+        }else{
+            $("ul.nav").slideUp();
+            $(this).addClass("fa-navicon").removeClass("fa-close");
+            $("nav").removeClass("mobileMenu");
+
+        }
+        isOpen = !isOpen
+    });
+    // move top
+    $('#goTop').click(function (e) {
+        e.preventDefault();
+        $('html').scrollTop(0);
+    })
+
 })
